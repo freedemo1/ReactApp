e6c5d3b77fa289591e4b4ea46b7ff62ea5c50a29
Group025 <Group025@infoway.com>
2022-11-10 15:38:13 +0530

    files react added

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World!!! 2022
+    </div>
+  );
+}
+
+export default App;
